2b449c91a15beb654c3373c7d60b1ab129129496
zhanghaokang <zhanghaokang@antiy.cn>
2020-10-16 11:01:09 +0800

    Develop | concurrent test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import json
+import threading
 
 from wisbec.date.time import TimeUtil
+from wisbec.logging.log import Log
 
 from src.testdata.test_data_generator import SmsSendDataGenerator, SmsInfo
 from src.util.post import PostUtil
@@ -10,6 +12,14 @@ class NoctorroTest:
     def __init__(self):
         self.m_send_sms_url: str = 'http://127.0.0.1:8089/modem_pool/send_sms'
         self.m_get_sms_url: str = 'http://127.0.0.1:8088/modem_pool/get_sms'
+        self.m_is_phone_num_available_url: str = 'http://127.0.0.1:8088/modem_pool/apply_for_phone_number'
+        self.m_lock: threading.Lock = threading.Lock()
+
+    def apply_for_phone_number(self, sms_info: SmsInfo) -> bool:
+        send_data = dict()
+        send_data['phoneNum'] = sms_info.m_receive_phone_num
+        r = PostUtil.post_form_data(self.m_is_phone_num_available_url, send_data)
+        return 'success' in r.text
 
     def send_sms(self, sms_info: SmsInfo) -> bool:
         send_data = dict()
@@ -25,22 +35,32 @@ class NoctorroTest:
         response_body = PostUtil.post_form_data(self.m_get_sms_url, send_data).text
         loaded = json.loads(response_body)
         if 'obj' not in loaded:
-            print('error')
+            Log.error('error response')
         else:
             sms = loaded['obj']
             if sms != sms_info.m_content:
-                print('error:', sms_info)
+                Log.error('error:{}', sms_info.m_content)
             else:
-                print('success')
+                Log.info('success')
+
+    def thread_test_case(self, sms_info: SmsInfo):
+        if self.apply_for_phone_number(sms_info):
+            sms_info.m_send_time = TimeUtil.now('%Y-%m-%d %H:%M:%S')
+            self.send_sms(sms_info)
+            self.get_sms(sms_info)
+        else:
+            Log.info('{} is busy', sms_info.m_receive_phone_num)
+
+    def test_main(self):
+        sms_info_list = SmsSendDataGenerator.generate_sms_info_list(10)
+        for sms_info in sms_info_list:
+            threading.Thread(target=self.thread_test_case, args=[sms_info]).start()
 
 
 def main():
-    sms_info_list = SmsSendDataGenerator.generate_sms_info_list(10)
+    Log.init_logger(is_log_to_file=False)
     noctorro_test = NoctorroTest()
-    for sms_info in sms_info_list:
-        sms_info.m_send_time = TimeUtil.now('%Y-%m-%d %H:%M:%S')
-        noctorro_test.send_sms(sms_info)
-        noctorro_test.get_sms(sms_info)
+    noctorro_test.test_main()
 
 
 if __name__ == '__main__':
